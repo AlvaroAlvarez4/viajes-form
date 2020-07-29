85cb04cd50ea39d5341ed74441c85b44e9729339
Álvaro Álvarez <68589916+AlvaroAlvarez4@users.noreply.github.com>
2020-07-29 17:15:29 +0200

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ViajesEditComponent } from './viajes-edit/viajes-edit.component';
+import { ViajesComponent } from './viajes/viajes.component';
+
+
+const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'viajes' },
+    { path: 'viajes', component: ViajesComponent },
+    { path: 'viajes-edit/:id', component: ViajesEditComponent },
+    { path: 'viajes-edit', component: ViajesEditComponent }
+]
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
